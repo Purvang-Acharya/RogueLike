1ced9601857f2c8db4550cc5d00eee3462fc7ebd
Purvang-Acharya <66854326+Purvang-Acharya@users.noreply.github.com>
2021-01-27 18:45:06 +0530

    player and enemy animator controller and scripts, basic game is playable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float turnDelay = 0.1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
 
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
     // Start is called before the first frame update
     void Awake()
     {
@@ -16,16 +21,47 @@ public class GameManager : MonoBehaviour
         else if (instance != this)
             Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
     void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (playersTurn || enemiesMoving)
+        {
+            return;
+        }
+        StartCoroutine(MoveEnemies());
+    }
+    public void GameOver()
+    {
+        enabled = false;
+    }
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        playersTurn = true;
+        enemiesMoving = false;
     }
 }
